ebb6227522b501e206a3d3e614da724633bd03ef
Gerard <gerardmccann94@hotmail.co.uk>
2023-08-22 16:59:12 +0100

    Creation of frontend directory

---
+++ frontend/react/src/App.jsx
@@ -0,0 +1,12 @@
+import UserProfile from "./UserProfile.jsx";
+function App() {
+
+
+  return (
+    <div>
+        <UserProfile/>
+    </div>
+  )
+}
+
+export default App
---
+++ frontend/react/src/UserProfile.jsx
@@ -0,0 +1,9 @@
+const UserProfile = () => {
+    return (
+        <div>
+            <img src={"https://randomuser.me/api/portraits/men/75.jpg"} />
+        </div>
+    )
+}
+
+export default UserProfile;
